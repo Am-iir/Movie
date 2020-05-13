37c128414a69729dfad94ea00fb6140e75c74a11
amir.maharjan <maharjanamir.101@gmail.com>
2020-05-13 19:41:04 +0545

    Popular Movies handled

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/','index');
+Route::get('/','MoviesController@index')->name('movies.index');
+Route::get('/show','MoviesController@show')->name('movies.show');
